177105484cffc17ac0f1318c961307ac4eb53cd0
DianaUdeA <diana.escobar3@udea.edu.co>
2024-08-21 22:50:46 -0500

    version1_correccionnombres

--- version1.py
+++ version1.py
@@ -13,59 +13,64 @@ class Paciente:
         return self.__genero
     def verCedula(self):
         return self.__cedula
-    
-    def asignarNombre(self,n):
+    def asignarNombre(self, n):
         self.__nombre = n
-    def asignarServicio(self,s):
+    def asignarServicio(self, s):
         self.__servicio = s
-    def asignarGenero(self,g):
+    def asignarGenero(self, g):
         self.__genero = g
-    def asignarCedula(self,c):
+    def asignarCedula(self, c):
         self.__cedula = c
 
 class Sistema:
     def __init__(self):
-        self.__listas_pacietes = []
-        self.__numero_pacientes = len(self.__lista_pacientes)
+        self.__lista_pacientes = [] 
+        self.__numero_pacientes = 0  
+
     def ingresarPaciente(self):
-        nombre = input("Ingrese e nombre: ")
-        cedula = int(input("Ingrese la CC: "))
-        genero = input("Ingrese el genero: ")
-        servicio = input("Ingreso el servicio: ")
+        nombre = input("Ingrese el nombre: ")
+        cedula = int(input("Ingrese la cédula: "))
+        genero = input("Ingrese el género: ")
+        servicio = input("Ingrese el servicio: ")
         p = Paciente()
         p.asignarNombre(nombre)
         p.asignarCedula(cedula)
         p.asignarGenero(genero)
         p.asignarServicio(servicio)
-        self.__lista_pacientes.append(p)
-        self.__numero_pacientes = len(self.__listas_pacietes)
+        self.__lista_pacientes.append(p)  
+        self.__numero_pacientes = len(self.__lista_pacientes)  
     def verNumeroPacientes(self):
         return self.__numero_pacientes
     def verDatosPaciente(self):
-        cedula = int(input("Ingrese la CC a buscar"))
+        cedula = int(input("Ingrese la cédula a buscar: "))
+        encontrado = False  # Variable para verificar si se encontró el paciente
         for paciente in self.__lista_pacientes:
             if cedula == paciente.verCedula():
                 print("Nombre: " + paciente.verNombre())
-                print("Cedula: " + str(paciente.verCedula()))
-                print("Genero: " + paciente.verGenero())
+                print("Cédula: " + str(paciente.verCedula()))
+                print("Género: " + paciente.verGenero())
                 print("Servicio: " + paciente.verServicio())
+                encontrado = True
+                break
+        if not encontrado:
+            print("Paciente no encontrado.")
 
 mi_sistema = Sistema()
 
 while True:
     opcion = int(input("""
-        1- Numero paciente
-        2- Numero de Pacientes
-        3- Datos paciente
+        1- Ingresar paciente
+        2- Ver número de pacientes
+        3- Ver datos de paciente
         4- Salir
         """))
     if opcion == 1:
         mi_sistema.ingresarPaciente()
-    elif opcion  == 2:
-        print("Ahora hay: "+ str(mi_sistema.verNumeroPacientes()))
+    elif opcion == 2:
+        print("Ahora hay: " + str(mi_sistema.verNumeroPacientes()))
     elif opcion == 3:
         mi_sistema.verDatosPaciente()
     elif opcion == 4:
         break
     else:
-        print("Opciòn no valida")
+        print("Opción no válida.")
